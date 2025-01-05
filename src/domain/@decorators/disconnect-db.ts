f97cfdeeb91cb0190e1b4a4d193c987ffbed0b87
Gabriel <gabriel.g2d2@gmail.com>
2025-01-05 19:51:23 -0300

    feat: enhance DisconnectDb decorator with environment-based logging for development

--- src/domain/@decorators/disconnect-db.ts
+++ src/domain/@decorators/disconnect-db.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
+import { env } from '@/env';
+
 export function DisconnectDb() {
   return function (
     target: any,
@@ -10,9 +12,17 @@ export function DisconnectDb() {
     const originalMethod = descriptor.value;
 
     descriptor.value = async function (...args: any[]) {
+      // TODO: remove this console.log
+      if (env.NODE_ENV === 'development') {
+        console.log(`Calling ${propertyKey}`);
+      }
       try {
         return await originalMethod.apply(this, args);
       } finally {
+        // TODO: remove this console.log
+        if (env.NODE_ENV === 'development') {
+          console.log(`Disconnecting from database for ${propertyKey}`);
+        }
         if (this.adapter && this.adapter.prisma) {
           await this.adapter.prisma.$disconnect();
         } else {
@@ -21,7 +31,6 @@ export function DisconnectDb() {
       }
     };
 
-    console.log('Disconnect decorator applied to: ', propertyKey);
     return descriptor;
   };
 }
