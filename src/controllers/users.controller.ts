6c5e9d1dfc8de4d4cd4a4c032714f7f3b218c18f
Gabriel <gabriel.g2d2@gmail.com>
2025-01-08 18:35:09 -0300

    feat: enhance user controller with Swagger documentation and dynamic DTO creation

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,11 +4,48 @@ import {
   UsersDomain,
 } from '@/domain/users/main';
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response } from 'express';
 
+export function createDto<T>(type: T): any {
+  class DynamicDto {
+    constructor() {
+      Object.keys(type).forEach((key) => {
+        this[key] = type[key];
+      });
+    }
+  }
+
+  Object.keys(type).forEach((key) => {
+    ApiProperty()(DynamicDto.prototype, key);
+  });
+
+  return DynamicDto;
+}
+
+const CreateUserDto = createDto<IDataCreateUserRequest>({
+  name: 'defaultUsername',
+  email: 'defaultEmail',
+  password: 'defaultPassword',
+});
+
+@ApiTags('users')
 @Controller('/users')
 export class UsersController {
   @Post('/create')
+  @ApiOperation({ summary: 'Create user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async create(@Body() body: IDataCreateUserRequest, @Res() reply: Response) {
     const domain = new UsersDomain();
     const result = await domain.createUser(body);
@@ -17,6 +54,12 @@ export class UsersController {
   }
 
   @Post('/session')
+  @ApiOperation({ summary: 'Authenticate user' })
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been successfully authenticated.',
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async authenticate(
     @Body() body: IDataAuthenticateRequest,
     @Res() reply: Response,
@@ -28,6 +71,12 @@ export class UsersController {
   }
 
   @Get('/profile/:userId')
+  @ApiOperation({ summary: 'Get user profile' })
+  @ApiResponse({
+    status: 200,
+    description: 'The user profile has been successfully retrieved.',
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   async getProfile(@Param('userId') userId: string, @Res() reply: Response) {
     const domain = new UsersDomain();
     const result = await domain.getProfile({
