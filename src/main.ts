67114dbce3adcfddf8f4398dad5919c91c712e4c
Gabriel <gabriel.g2d2@gmail.com>
2025-01-08 18:35:05 -0300

    feat: add Swagger documentation setup for MyGym API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // TODO: remove the comment below to enable the Swagger documentation
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(process.env.PORT ?? 3333);
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MyGym API')
+    .setDescription('The MyGym API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
