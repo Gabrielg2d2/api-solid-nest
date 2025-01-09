8ad0190aaa07a617fbc2ea480a1652c7905a1f36
Gabriel <gabriel.g2d2@gmail.com>
2025-01-09 17:51:14 -0300

    feat: enhance API documentation with environment-specific descriptions

--- src/main.ts
+++ src/main.ts
@@ -11,29 +11,32 @@ function getNodeEnv() {
         title: 'MyGym API - TEST',
         name: 'test',
         port: 4000,
+        description: 'The MyGym API documentation, for testing purposes',
       };
     case 'development':
       return {
         title: 'MyGym API - DEV',
         name: 'development',
         port: process.env.PORT || 3333,
+        description: 'The MyGym API documentation, for development purposes',
       };
     default:
       return {
         title: 'MyGym API - PROD',
         name: 'production',
         port: process.env.PORT || 3333,
+        description: 'The MyGym API documentation, for production purposes',
       };
   }
 }
 
 async function bootstrap() {
-  const { title, port } = getNodeEnv();
+  const { title, port, description } = getNodeEnv();
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle(title)
-    .setDescription('The MyGym API documentation')
+    .setDescription(description)
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
