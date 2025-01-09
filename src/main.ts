c4d746bee427e2e1eca5a01a24d29ebe794738f3
Gabriel <gabriel.g2d2@gmail.com>
2025-01-09 17:49:45 -0300

    feat: implement environment-specific configurations for MyGym API

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const isTest = process.env.NODE_ENV === 'test';
-  const app = await NestFactory.create(AppModule);
+function getNodeEnv() {
+  const key = process.env.NODE_ENV;
 
-  const title = isTest ? '** MyGym API-Test **' : 'MyGym API';
+  switch (key) {
+    case 'test':
+      return {
+        title: 'MyGym API - TEST',
+        name: 'test',
+        port: 4000,
+      };
+    case 'development':
+      return {
+        title: 'MyGym API - DEV',
+        name: 'development',
+        port: process.env.PORT || 3333,
+      };
+    default:
+      return {
+        title: 'MyGym API - PROD',
+        name: 'production',
+        port: process.env.PORT || 3333,
+      };
+  }
+}
+
+async function bootstrap() {
+  const { title, port } = getNodeEnv();
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle(title)
@@ -16,7 +39,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = isTest ? 4000 : process.env.PORT || 3333;
   await app.listen(port);
 }
 bootstrap();
