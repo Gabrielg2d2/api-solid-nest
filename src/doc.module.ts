913c7b64a84f26466b1f9ce926d6677c81c8b729
Gabriel <gabriel.g2d2@gmail.com>
2025-01-10 13:36:47 -0300

    feat: update import path for DocUsersController to reflect new file structure

--- src/doc.module.ts
+++ src/doc.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DocUsersController } from './controllers/users/doc-users.controller.test';
+import { DocUsersController } from './controllers/users/doc.controller.test';
 
 @Module({
   imports: [],
