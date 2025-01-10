7d07c9be5172bfa96bb84b75c812e4eda103d171
Gabriel <gabriel.g2d2@gmail.com>
2025-01-10 14:23:18 -0300

    feat: add header validation middleware to enhance security in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
+import { ValidateHeaderMiddleware } from './middlewares/header';
 
 @Module({
   imports: [],
   controllers: [UsersController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateHeaderMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
---
+++ src/middlewares/header/index.ts
@@ -0,0 +1,19 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class ValidateHeaderMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const headerValue = req.headers['header'];
+
+    if (!headerValue || headerValue !== '123') {
+      throw new UnauthorizedException('Invalid or missing header');
+    }
+
+    next();
+  }
+}
