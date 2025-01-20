9f9848f8590074c8b576c4c04dbaf6c1fd9d29e8
Gabriel <gabriel.g2d2@gmail.com>
2025-01-20 20:32:02 -0300

    refactor: rename FactoryUsersDomainTest to FactoryUsersDomain for clarity

--- src/application/factories/users/domain.ts
+++ src/application/factories/users/domain.ts
@@ -1,7 +1,7 @@
 import { UsersDomain } from '../../domains/users/main';
 import { RepositoryUsers } from '../../domains/users/repositories/repository';
 
-export class FactoryUsersDomainTest {
+export class FactoryUsersDomain {
   static create() {
     const repository = new RepositoryUsers();
     return new UsersDomain(repository);
