c6b09043cb90899b215d39cf1b61e01430d4439c
Gabriel <gabriel.g2d2@gmail.com>
2025-01-20 20:31:19 -0300

    refactor: change create method in FactoryUsersDomainTest to synchronous

--- src/application/factories/users/domain.test.ts
+++ src/application/factories/users/domain.test.ts
@@ -2,7 +2,7 @@ import { UsersDomain } from '../../domains/users/main';
 import { RepositoryUserTest } from '../../domains/users/repositories/repository-test';
 
 export class FactoryUsersDomainTest {
-  static async create() {
+  static create() {
     const repository = new RepositoryUserTest();
     return new UsersDomain(repository);
   }
--- src/application/factories/users/domain.ts
+++ src/application/factories/users/domain.ts
@@ -2,7 +2,7 @@ import { UsersDomain } from '../../domains/users/main';
 import { RepositoryUsers } from '../../domains/users/repositories/repository';
 
 export class FactoryUsersDomainTest {
-  static async create() {
+  static create() {
     const repository = new RepositoryUsers();
     return new UsersDomain(repository);
   }
