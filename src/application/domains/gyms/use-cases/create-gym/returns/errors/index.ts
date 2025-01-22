7adaf2218ff79d9caaa9e5bd0e42a5ead14ccb85
Gabriel <gabriel.g2d2@gmail.com>
2025-01-22 18:55:48 -0300

    refactor: simplify ReturnError class by removing unnecessary interface implementation

--- src/application/domains/gyms/use-cases/create-gym/returns/errors/index.ts
+++ src/application/domains/gyms/use-cases/create-gym/returns/errors/index.ts
@@ -1,11 +1,6 @@
 import { PresenterErrorGlobal } from '@/application/@global/class/presenter/error';
-import { IReturnDefaultDomainGlobal } from '@/application/@global/types/return-default-domain';
 
-interface IReturnError {
-  execute(error: Error | unknown): Promise<IReturnDefaultDomainGlobal<null>>;
-}
-
-export class ReturnError extends PresenterErrorGlobal implements IReturnError {
+export class ReturnError extends PresenterErrorGlobal {
   constructor() {
     super();
   }
