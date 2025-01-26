260198973abc609fba1b79e049f39bb55385ac67
Gabriel <gabriel.g2d2@gmail.com>
2025-01-26 19:41:30 -0300

    refactor: update create and fetchHistoryCheckIns methods to return data in a structured format

--- src/controllers/check-ins/base.controller.ts
+++ src/controllers/check-ins/base.controller.ts
@@ -4,8 +4,7 @@ import {
 } from '@/application/domains/checkins/main';
 import { CreateCheckInsDocs } from '@/doc/checkIns/create-check-ins';
 import { HistoryCheckInsDocs } from '@/doc/checkIns/fetch-historys-check-ins';
-import { Body, Get, Headers, Param, Post, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Body, Get, Headers, Param, Post } from '@nestjs/common';
 
 export class BaseCheckInsController {
   protected domain!: CheckInDomain;
@@ -15,22 +14,22 @@ export class BaseCheckInsController {
   async create(
     @Headers('header') headerValue = 'default-value',
     @Body() body: IDataCreateRequest,
-    @Res() reply: Response,
   ) {
     console.log('Header value: ', headerValue);
     const data = await this.domain.create(body);
 
-    return reply.send(data);
+    return {
+      checkIn: data,
+    };
   }
 
   @Get('/history-check-ins/:userId')
   @HistoryCheckInsDocs()
-  async fetchHistoryCheckIns(
-    @Param('userId') userId: string,
-    @Res() reply: Response,
-  ) {
+  async fetchHistoryCheckIns(@Param('userId') userId: string) {
     const data = await this.domain.fetchHistoryCheckIns(userId);
 
-    return reply.send(data);
+    return {
+      historyCheckIns: data,
+    };
   }
 }
