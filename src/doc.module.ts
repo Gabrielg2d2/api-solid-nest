d5fb033fae9747b7ffab3a38cf070ad680b6f224
Gabriel <gabriel.g2d2@gmail.com>
2025-01-28 18:07:51 -0300

    refactor: remove SetHeader decorator and update import paths for documentation controllers

--- src/doc.module.ts
+++ src/doc.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DocCheckInsController } from './controllers/check-ins/doc.controller.test';
-import { DocGymsController } from './controllers/gyms/doc.controller.test';
-import { DocUsersController } from './controllers/users/doc.controller.test';
+import { DocCheckInsController } from './controllers/check-ins/doc';
+import { DocGymsController } from './controllers/gyms/doc';
+import { DocUsersController } from './controllers/users/doc';
 
 @Module({
   imports: [],
