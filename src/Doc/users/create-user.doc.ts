0e30dc1a4370cd7ac95230b82e4044e88d80c3d1
Gabriel <gabriel.g2d2@gmail.com>
2025-02-01 18:49:08 -0300

    refactor: update user creation documentation to include detailed request and response models

--- src/Doc/users/create-user.doc.ts
+++ src/Doc/users/create-user.doc.ts
@@ -1,16 +1,39 @@
-import { IDataCreateUserRequest } from '@/application/domains/users/main';
 import { applyDecorators } from '@nestjs/common';
-import { ApiBody, ApiHeader, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { useGenerateDoc } from '../@functions/useGenerateDoc';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+} from '@nestjs/swagger';
 
-const CreateUserDto = useGenerateDoc<IDataCreateUserRequest>(
-  {
-    name: 'Name Test',
-    email: 'test@gmail.com',
-    password: '123456',
-  },
-  'CreateUserDto',
-);
+class CreateUserRequest {
+  @ApiProperty({ example: 'John Doe' })
+  name!: string;
+
+  @ApiProperty({ example: 'john.doe@example.com' })
+  email!: string;
+
+  @ApiProperty({ example: 'password123' })
+  password!: string;
+}
+
+class CreateUserResponse {
+  @ApiProperty({ example: '1' })
+  id!: string;
+
+  @ApiProperty({ example: 'John Doe' })
+  name!: string;
+
+  @ApiProperty({ example: 'john.doe@example.com' })
+  email!: string;
+
+  @ApiProperty({ example: 'hashedpassword123' })
+  password_hash!: string;
+
+  @ApiProperty({ example: '2023-10-01T00:00:00Z' })
+  created_at!: string;
+}
 
 export function CreateUserDocs() {
   return applyDecorators(
@@ -20,10 +43,11 @@ export function CreateUserDocs() {
       description: 'Description header',
       required: false,
     }),
-    ApiBody({ type: CreateUserDto }),
+    ApiBody({ type: CreateUserRequest }),
     ApiResponse({
       status: 201,
       description: 'The user has been successfully created.',
+      type: CreateUserResponse,
     }),
     ApiResponse({ status: 400, description: 'Bad Request.' }),
     ApiResponse({ status: 409, description: 'User already exists' }),
