3918afdc0632796251c0160287a3a5939802d4f0
Gabriel <gabriel.g2d2@gmail.com>
2025-02-01 20:42:50 -0300

    refactor: streamline useGenerateDoc function by removing commented code and enhancing array handling

--- src/Doc/@functions/useGenerateDoc/index.ts
+++ src/Doc/@functions/useGenerateDoc/index.ts
@@ -27,49 +27,6 @@
 // }
 
 // TODO: test new function1
-// import { ApiProperty } from '@nestjs/swagger';
-
-// export function useGenerateDoc<T extends object>(type: T, dtoName: string) {
-//   const DynamicDto = class {
-//     constructor() {
-//       Object.keys(type).forEach((key) => {
-//         this[key] = type[key];
-//       });
-//     }
-//   };
-
-//   if (Array.isArray(type)) {
-//     const itemType = type.length > 0 ? type[0] : {};
-//     const DynamicDto = useGenerateDoc(itemType, dtoName);
-//     return [DynamicDto];
-//   }
-
-//   Object.defineProperty(DynamicDto, 'name', { value: dtoName });
-
-//   Object.keys(type).forEach((key) => {
-//     let propertyType;
-
-//     if (Array.isArray(type[key])) {
-//       propertyType =
-//         type[key].length > 0
-//           ? [useGenerateDoc(type[key][0], `${dtoName} => ${key}`)]
-//           : Array;
-//     } else if (typeof type[key] === 'object' && type[key] !== null) {
-//       propertyType = useGenerateDoc(type[key], `${dtoName} => ${key}`);
-//     } else {
-//       propertyType = type[key].constructor;
-//     }
-
-//     ApiProperty({
-//       example: type[key],
-//       type: propertyType,
-//     })(DynamicDto.prototype, key);
-//   });
-
-//   return DynamicDto;
-// }
-
-// TODO: test new function2
 import { ApiProperty } from '@nestjs/swagger';
 
 export function useGenerateDoc<T extends object>(type: T, dtoName: string) {
@@ -80,6 +37,12 @@ export function useGenerateDoc<T extends object>(type: T, dtoName: string) {
       });
     }
   };
+
+  if (Array.isArray(type)) {
+    const itemType = type.length > 0 ? type[0] : {};
+    const DynamicDto = useGenerateDoc(itemType, dtoName);
+    return [DynamicDto];
+  }
 
   Object.defineProperty(DynamicDto, 'name', { value: dtoName });
 
